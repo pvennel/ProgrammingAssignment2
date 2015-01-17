0ea8e5f743b3426415058896fef3eb230559342d
pvennel <vfpeter@gmail.com>
2015-01-17 15:51:40 -0500

    Modified makeCacheMatrix and cacheSolve
    Added code in both functions to get the desired results. Tested with
    three sets of data for consistency.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix() allows for creation of a list for gettin and setting Matrix at global level
+## cacheSolve() provides functionality to return the Inverse of the Matrix from cache if existing.
 
-## Write a short comment describing this function
+## The function makeCacheMatrix() creates a list containg function to perform the following :
+##         - set value of the Matrix
+##         - get value of the Matrix
+##         - set value representing Inverse of the Matrix
+##         - get value representing Inverse of the Matrix
+##  Super Assignment operator "<<-" is used to set the values at golbal level so that the values can be
+##  cached.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inv <- NULL
+  
+  ## creates a global variable with the input Matrix and thus caches it.
+  set <- function(y) {
+     x <<- y
+     inv <<- NULL
+  }
+  
+  ## returns back the cached input Matrix
+  get <- function() x
+  
+  ## create a global variable containing the Inverse MAtrix and thus caches the data
+  setInv <- function(invMtx)inv <<- invMtx
+  
+  ## returns back the cached Inverse MAtrix
+  getInv <- function() inv
+  
+  ## list containing getters and setters are return by this function
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)  
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve accepts a Matrix as input and returns back the Inverse Matrix
+## It should be noted that the Inverse is computed only once for a given Matrix. If the 
+## value is found in globally, then that is instantly returned saving the time to computer 
+## the Inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## reading the Inverse Matrix data (it might or might not exist)
+  inv <- x$getInv()
+  
+  ## checks to see if a non null Matrix containing Inverse data is returned.
+  ## if found then we exit from this function with that value.
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return (inv)
+  }
+  
+  
+  data <- x$get()
+  
+  ## use solve() to compute the inverse of the matrix
+  inv <- solve(data, ...)
+  
+  message("setting the Inverse data for first time")
+  x$setInv(inv)
+  
+  ## return to the caller, as last line is always the function result
+  inv
+  
 }
